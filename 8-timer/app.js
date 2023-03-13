919d6d52b6f1852ed35765be1e0df2c32957d668
yeonkyoungpark <yyeon111@naver.com>
2023-03-13 16:09:55 +0900

    timer APIS with process.API

--- 8-timer/app.js
+++ 8-timer/app.js
@@ -1,8 +1,30 @@
-let num = 1;
-// setInterval(()=>{
-//   console.log(num++);
-// },1000);
+// let num = 1;
 
-setTimeout(()=>{
-  console.log(num++);
-},1000);
+// const interval = setInterval(() => {
+//   console.log(num++);
+// }, 1000);
+
+// setTimeout(() => {
+//   console.log('Time OUT!');
+//   clearInterval(interval);
+// }, 6000);
+
+// console.time('im')
+// setImmediate(()=>{
+//   console.timeEnd('im');
+//   console.log('Immediately!');
+//   }
+// )
+
+console.time('set')
+setTimeout(() => {
+  console.timeEnd('set');
+  console.log('TimeOut!');
+},0)
+
+console.time('im')
+setImmediate(()=>{
+  console.timeEnd('im');
+  console.log('Immediately!');
+  }
+)
