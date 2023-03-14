d39d6b1c6aa72bdb3658e9aaeab1b4c82bcc0e8d
yeonkyoungpark <yyeon111@naver.com>
2023-03-14 19:19:18 +0900

    Buffer class, API

--- 11-buffer/app.js
+++ 11-buffer/app.js
@@ -1,17 +1,21 @@
+const fs = require('fs');
+
 const buf = Buffer.from('Hi');
-console.log(buf);
 console.log(buf.length);
-console.log(buf.toString());
 console.log(buf[0]);
 console.log(buf[1]);
+console.log(buf.toString());
 
 const buf2 = Buffer.alloc(2);
-const buf3 = Buffer.allocUnsafe(2); 
-buf2[0]=72;
-buf2[1]=105;
+const buf3 = Buffer.allocUnsafe(2);
+console.log(buf2, buf3);
+
+buf2[0] = 72;
+buf2[1] = 105;
+console.log(buf2);
 buf2.copy(buf3);
 console.log(buf2.toString());
-console.log(buf3.toString());
+console.log(buf2.toString());
 
-const newBuf = Buffer.concat([buf,buf2,buf3]);
+const newBuf = Buffer.concat([buf, buf2, buf3]);
 console.log(newBuf.toString());
